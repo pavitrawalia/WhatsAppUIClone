911cdfb2944f23f6bd5e002f1d9f50f41b5cbca1
Pavitra Walia <pavitrawalia@Pavitras-MacBook-Air.local>
2025-06-29 20:52:13 +0530

    Fake data for chats support added using faker-js

---
+++ src/generateFakeData.js
@@ -0,0 +1,44 @@
+import { Faker, en } from '@faker-js/faker';
+
+const faker = new Faker({ locale: en });
+
+class User {
+  constructor() {
+    this.id = faker.string.uuid();
+    this.name = faker.person.fullName();
+    this.profilePicture = faker.image.avatar();
+  }
+}
+
+export class Message {
+  constructor(isMainUser, msg, date) {
+    this.id = faker.string.uuid();
+    this.msg = msg ||
+  faker.helpers.arrayElement([
+    "Hey, how are you doing?",
+    "Let’s meet after work.",
+    "I forgot to tell you something!",
+    "Guess what happened today?",
+    "It’s been a long day.",
+    "Wanna grab coffee?",
+    "I'm on my way.",
+    "That’s awesome news!",
+    "You’re kidding, right?",
+    "Seriously, what the heck!"
+  ]);
+    this.isMainUser = isMainUser;
+    this.date = date || faker.date.recent();
+  }
+}
+
+export const mainUser = new User();
+
+export const contacts = Array.from({ length: 15 }, () => new User());
+
+export const contactsMessages = contacts.map((contact) => {
+  const messages = Array.from({ length: 50 }, (_, i) =>
+    (i + 1) % 2 === 0 ? new Message(true) : new Message(false)
+  ).filter((m) => m.msg);
+
+  return { contact, messages };
+});
